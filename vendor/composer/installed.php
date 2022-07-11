ec61b0f8d2475fe3628947e606b2344283c80e80
skooijman <bas.kooijman@gmail.com>
2022-07-11 16:37:51 +0200

    lockfile created

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'atlantisgroupbv/ormmodel',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '973b94bc961b18d8d1aea70e0dc3f735ace4b870',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'atlantisgroupbv/ormmodel' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '973b94bc961b18d8d1aea70e0dc3f735ace4b870',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
